01df63a355a1c648094ce0d584e3a3d1c2fb3dc2
Konstantin Borisov <effectumka@gmail.com>
2024-02-19 20:53:48 +0300

    Был сделан минимальный стартовый телеграм бот

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+import telebot
+
+# Указываем токен вашего бота, который вы получили у BotFather
+TOKEN = '6903824564:AAHdkswkz44FizTwrcRcz1zKyt1W_VC5BL8'
+
+# Создаем экземпляр бота
+bot = telebot.TeleBot(TOKEN)
+
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Приветствую")
+
+# Запускаем бота
+bot.polling()
