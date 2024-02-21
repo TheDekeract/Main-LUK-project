4dc07d46ee0ad3b795c2259044345f54baaacba1
Борис <dersiegistunserbrat@gmail.com>
2024-02-21 22:24:39 +0300

    Смайлики добавлены.

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ import telebot
 from telebot.async_telebot import AsyncTeleBot
 
 # Указываем токен вашего бота, который вы получили у BotFather Создаем экземпляр бота
-bot = AsyncTeleBot('6524851966:AAH8Sv5CO16nLanHmMthGPOoTqmwpB5ecNU')
+bot = AsyncTeleBot('6947679713:AAEkzjA8Qv5gbB7iocqldIhc9Lg3OXbMC5A')
 
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start', 'menu'])
 async def handle_start(message):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    lessons0 = telebot.types.KeyboardButton('Расписание')
-    settings0 = telebot.types.KeyboardButton('Настройки')
-    findname0 = telebot.types.KeyboardButton('Поиск преподавателя')
-    help0 = telebot.types.KeyboardButton('Помощь')
+    lessons0 = telebot.types.KeyboardButton('📖Расписание')
+    settings0 = telebot.types.KeyboardButton('⚙️Настройки')
+    findname0 = telebot.types.KeyboardButton('👨🏻‍💼Поиск преподавателя')
+    help0 = telebot.types.KeyboardButton('🆘Помощь')
     markup.add(lessons0)
     markup.add(settings0)
     markup.add(findname0)
     markup.add(help0)
-    await bot.send_message(message.chat.id, "Приветствую", reply_markup=markup)
+    await bot.send_message(message.chat.id, "Приветствую, я чат бот с расписанием! Что тебя интересует?", reply_markup=markup)
 
 
-@bot.message_handler(func=lambda message: message.text == 'Расписание')
+@bot.message_handler(func=lambda message: message.text == '📖Расписание')
 async def lessons(message):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    mylessons = telebot.types.KeyboardButton('Моё расписание')
-    alllessons = telebot.types.KeyboardButton('Расписание курса')
+    mylessons = telebot.types.KeyboardButton('🗓Моё расписание')
+    alllessons = telebot.types.KeyboardButton('📚Расписание курса')
     menu = telebot.types.KeyboardButton('/menu')
     markup.add(mylessons)
     markup.add(alllessons)
@@ -33,12 +33,12 @@ async def lessons(message):
     await bot.send_message(message.chat.id, 'Здесь будет расписание', reply_markup=markup)
 
 
-@bot.message_handler(func=lambda message: message.text == 'Настройки', )
+@bot.message_handler(func=lambda message: message.text == '⚙️Настройки', )
 async def settings(message):
     markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    changegr = telebot.types.KeyboardButton('Сменить группу')
-    changentf = telebot.types.KeyboardButton('Сменить уведомления')
-    changeweek = telebot.types.KeyboardButton('Сменить неделю на следующую')
+    changegr = telebot.types.KeyboardButton('👨🏻‍🎓Сменить группу')
+    changentf = telebot.types.KeyboardButton('🔔Сменить уведомления')
+    changeweek = telebot.types.KeyboardButton('↔️Сменить неделю на следующую')
     menu = telebot.types.KeyboardButton('/menu')
     markup.add(changegr)
     markup.add(changentf)
@@ -47,7 +47,7 @@ async def settings(message):
     await bot.send_message(message.chat.id, 'Здесь будут настройки', reply_markup=markup)
 
 
-@bot.message_handler(func=lambda message: message.text == 'Поиск преподавателя')
+@bot.message_handler(func=lambda message: message.text == '👨🏻‍💼Поиск преподавателя')
 async def findname(message):
     markup = telebot.types.ReplyKeyboardMarkup()
     menu = telebot.types.KeyboardButton('/menu')
@@ -55,18 +55,18 @@ async def findname(message):
     await bot.send_message(message.chat.id, 'Здесь будет поиск по именам')
 
 
-@bot.message_handler(func=lambda message: message.text == 'Помощь')
+@bot.message_handler(func=lambda message: message.text == '🆘Помощь')
 async def help(message):
     await bot.send_message(message.chat.id,
-                     '''  Справка для бота.
-Бот предоставляет услуги по уведомлению расписаний в беседах и личных сообщениях.
-В разделе \"Расписание\" можно найти кнопки, предоставляющие \"Ваше расписание\" и \"Общее расписание колледжа\".
-\"Ваше расписание\" является расписанием для вами выбранной группы.
-Кнопка \"Преподаватель\" позволяет найти преподавателя в расписании по ФИО.
-В разделе \"Настройки\" присутствуют кнопки, изменяющие выбранную группу, изменяющие частоту уведомления, а также обновление недели общего расписания (по возможности).
+                     f''' ❗ Справка для бота.
+🤖Бот предоставляет услуги по уведомлению расписаний в беседах и личных сообщениях.
+В разделе \"Расписание 📖\" можно найти кнопки, предоставляющие \"Ваше расписание\" и \"Общее расписание колледжа\".
+\"Ваше расписание🗓\" является расписанием для вами выбранной группы.
+Кнопка \"Преподаватель 👨🏻‍💼\" позволяет найти преподавателя в расписании по ФИО.
+В разделе \"Настройки ⚙️\" присутствуют кнопки, изменяющие выбранную группу, изменяющие частоту уведомления, а также обновление🔄 недели общего расписания (по возможности).
 Обновление недели может произойти только тогда, когда в бот была добавлена следующая неделя, а воскресенье еще не наступило.
 В воскресенье неделя сменяется самостоятельно и для всех пользователей.
-Удачи в обучении. Спасибо, что пользуйтесь нашим ботом!''')
+🎉 Удачи в обучении. Спасибо, что пользуйтесь нашим ботом!''')
 
 
 # Запускаем бота
